c3100dea2acc6b3e6796e03f772bd1e44cee5a36
Ângelo Lucas <angelo.lrc@gmail.com>
2018-09-24 23:59:42 -0300

    Imported from my codepen

---
+++ script.js
@@ -0,0 +1,70 @@
+/*
+JS used for camera rotation
+*/
+$(document).ready(function() {
+  var winW = $(window).width()
+  var winH = $(window).height()
+  var $demo = $('.platform-3d__rotate')
+  var rotation = {
+    x: 45,
+    z: 225,
+  }
+  var prevValues = {
+    x: 90,
+    y: 45,
+  }
+  var rotationActive = false
+  var startAnimTime = 5400
+  var timeout = setTimeout(function() {
+    $demo.addClass('ready')
+  }, 4400)
+
+  $(document).on('mousedown', function(e) {
+    var startX = e.pageX
+    var startY = e.pageY
+
+    $(document).on('mousemove', function(e) {
+      var deltaX
+      var deltaY
+      if (!rotationActive) {
+        deltaX = startX - e.pageX
+        deltaY = startY - e.pageY
+        rotationActive = true
+      } else {
+        deltaX = prevValues.x - e.pageX
+        deltaY = prevValues.y - e.pageY
+      }
+      prevValues.x = e.pageX
+      prevValues.y = e.pageY
+
+      var degX = (deltaX * 100) / 360
+      var degY = (deltaY * 100) / 360
+
+      rotation.x += degY
+      rotation.z += degX
+
+      $demo.css(
+        'transform',
+        'rotateX(' + rotation.x + 'deg) rotateZ(' + rotation.z + 'deg)'
+      )
+    })
+
+    $(document).on('mouseup', function(e) {
+      $(document).off('mousemove')
+      $(document).off('mouseup')
+      rotationActive = false
+      prevValues.x = 0
+      prevValues.y = 0
+    })
+  })
+
+  $('.magic-button').click(function() {
+    $('.background').toggleClass('magic')
+  })
+
+  $(document).keypress(function(event) {
+    if (event.originalEvent.key === 'm') {
+      $('.background').toggleClass('magic')
+    }
+  })
+})
