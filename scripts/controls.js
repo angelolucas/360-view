99c373d1be0001a72f2936a61659cc7fd44614de
Ângelo Lucas <angelo.lrc@gmail.com>
2018-09-29 18:19:40 -0300

    Disable zoom on last step

--- scripts/controls.js
+++ scripts/controls.js
@@ -3,44 +3,53 @@ var select = {
   input: {
     showGrid: document.querySelector('.input-show-grid'),
     cubeBorder: document.querySelector('.input-cube-border'),
-    zoomIn: document.querySelector('.zoom-in'),
-    zoomOut: document.querySelector('.zoom-out'),
+    zoomIn: document.querySelector('.input-zoom[data-zoom="in"]'),
+    zoomOut: document.querySelector('.input-zoom[data-zoom="out"]'),
   },
 }
 var zoomSteps = [0.75, 1, 1.5, 2, 3, 5, 10, 50]
 var currentZoom = zoomSteps.length - 1
 
+// Toggle Show Grid
 select.input.showGrid.onchange = function() {
   select.cubemap.classList.toggle('show-grid')
 }
 
+// Toogle Cube Borders
 select.input.cubeBorder.onchange = function() {
   select.cubemap.classList.toggle('cube-border')
 }
 
+// Zoom Control
 select.input.zoomIn.onclick = function() {
-  var lastStap = currentZoom === zoomSteps.length - 1
-
-  if (!lastStap) {
-    currentZoom++
-    applyZoom()
-  }
+  handleZoom('in')
+  disableZoomOnLastStep()
 }
 
 select.input.zoomOut.onclick = function() {
-  var lastStep = currentZoom === 0
-
-  if (!lastStep) {
-    currentZoom--
-    applyZoom()
-  }
+  handleZoom('out')
+  disableZoomOnLastStep()
 }
 
-var applyZoom = function() {
+var handleZoom = function(direction) {
+  if (direction === 'in') currentZoom++
+  if (direction === 'out') currentZoom--
+
   var scale = zoomSteps[currentZoom]
 
   select.cubemap.style.transform =
     'scale3d(' + scale + ',' + scale + ',' + scale + ')'
 }
 
-document.onload = applyZoom()
+var disableZoomOnLastStep = function() {
+  if (currentZoom === zoomSteps.length - 1) {
+    select.input.zoomIn.disabled = true
+  } else if (currentZoom === 0) {
+    select.input.zoomOut.disabled = true
+  } else {
+    select.input.zoomIn.disabled = false
+    select.input.zoomOut.disabled = false
+  }
+}
+
+document.onload = [handleZoom(), disableZoomOnLastStep()]
