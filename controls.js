41995b0ebaeb38d253adf1713bba2cb19381a47d
Ângelo Lucas <angelo.lrc@gmail.com>
2018-09-29 14:18:17 -0300

    Styled controls and zoom steps

--- controls.js
+++ controls.js
@@ -3,19 +3,47 @@ var select = {
 
   input: {
     showGrid: document.querySelector('.input-show-grid'),
-    zoomOut: document.querySelector('.input-zoom-out'),
     cubeBorder: document.querySelector('.input-cube-border'),
+    zoomIn: document.querySelector('.zoom-in'),
+    zoomOut: document.querySelector('.zoom-out'),
   },
 }
+var zoomSteps = [0.75, 1, 1.5, 2, 3, 5, 10, 50]
+var currentZoom = 7
 
 select.input.showGrid.onchange = function() {
   select.cubemap.classList.toggle('show-grid')
 }
 
-select.input.zoomOut.onchange = function() {
-  select.cubemap.classList.toggle('zoom-out')
-}
-
 select.input.cubeBorder.onchange = function() {
   select.cubemap.classList.toggle('cube-border')
 }
+
+select.input.zoomIn.onclick = function() {
+  if (currentZoom < zoomSteps.length - 1) {
+    currentZoom++
+
+    applyZoom()
+  }
+}
+
+select.input.zoomOut.onclick = function() {
+  if (currentZoom > 0) {
+    currentZoom--
+
+    applyZoom()
+  }
+}
+
+var applyZoom = function() {
+  select.cubemap.style.transform =
+    'scale3d(' +
+    zoomSteps[currentZoom] +
+    ',' +
+    zoomSteps[currentZoom] +
+    ',' +
+    zoomSteps[currentZoom] +
+    ')'
+}
+
+document.onload = applyZoom()
